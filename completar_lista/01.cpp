277acb29e81f2144ae49c1ddb37ceeaea69b4bdd
italoprobo <71471583+italoprobo@users.noreply.github.com>
2022-10-13 16:58:24 -0300

    Update 01.cpp

--- completar_lista/01.cpp
+++ completar_lista/01.cpp
@@ -12,41 +12,52 @@ Aluno lista[MAX];
 
 
 void incluirDesordenado(Aluno e){
-    int alunos = 0; int count;
+    int alunos = 0;
 
     
     for(int i = 0; i < MAX; i++){
+        if(alunos < MAX){
         printf("\n Matricula aluno: \n");
         scanf("%d", &lista[i].mat);
         printf("\n Nome aluno: \n");
         scanf("%s", &lista[i].nome);
         alunos++;
+        }
+        else{
+            printf("\n Cheia!");
+        }
     }
 }
 
 //retorna a posicao do elemento procurado
-int procura(int mat){	
+int procura(int mat){
+    for (int i = 0; i < MAX; i++){
+        if(lista[i].mat == mat){
+            return i;
+            break;
+        }else{
+            return -1;
+        }
+    }
+    
 }
 
 //recebe a posicao e imprime o elemento na tela
 void mostrar(int pos){
-	
+	printf("%d", &lista[pos].mat);
+	printf("%s", &lista[pos].nome);
 }
 
 //procura o elemento e depois mostra o elemento encontrado (se nao for encontrado informa q ele nao existe)
-void consultar(int mat){	
+void consultar(int mat){
+        for (int i = 0; i < MAX; i++){
+        if(lista[i].mat == mat){
+            mostrar(i);
+            break;
+        }else if(lista[i].mat == NULL){
+            printf("\n Não existe!");
+            break;
+        }
+    }
 }
 
-//estrat�gia 1: colocar o ultimo elemento da lista na posi��o do elemento removido
-void remover00(){
-	
-}
-
-//estrat�gia 2: mover TODOS os elementos que est�o AP�S  o elemento que deve ser removido UMA POSI��O A FRENTE.
-void remover01(){
-}
-
-main(){
-	
-	
-}
